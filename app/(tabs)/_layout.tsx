d6f060f08c995e5ca2ff666a94cccff4c7ec86fd
obaidallah <obaidalqershi1@gmail.com>
2025-04-08 06:09:55 +0300

    final commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -73,7 +73,7 @@ export default function TabsLayout() {
       />
 
       <Tabs.Screen
-        name="save"
+        name="saved"
         options={{
           title: "Save",
           headerShown: false,
